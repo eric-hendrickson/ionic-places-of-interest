6069599cb94160f03d042bf4c5419c30ba9cdf60
Eric Hendrickson <hendrickson02@gmail.com>
2018-08-19 16:11:11 -0700

    Added provider and dependencies

--- native/src/app/app.module.ts
+++ native/src/app/app.module.ts
@@ -8,27 +8,29 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MapsProvider } from '../providers/maps/maps';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+	declarations: [
+		MyApp,
+		HomePage,
+		ListPage
+	],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(MyApp),
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		HomePage,
+		ListPage
+	],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{ provide: ErrorHandler, useClass: IonicErrorHandler },
+    MapsProvider
+	]
 })
-export class AppModule {}
+export class AppModule { }
