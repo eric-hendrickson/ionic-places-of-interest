22ad729bb0dac14a8552d80ea5e680efd47e18fa
Eric Hendrickson <hendrickson02@gmail.com>
2018-08-20 20:23:55 -0700

    Embedded map with Geolocation, will try to reset to google maps

--- native/src/pages/home/home.ts
+++ native/src/pages/home/home.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google: any;
+
+const options = {
+	enableHighAccuracy: true,
+	timeout: 5000,
+	maximumAge: 0
+  };
 
 @Component({
 	selector: 'page-home',
@@ -8,15 +16,20 @@ import { Geolocation } from '@ionic-native/geolocation';
 })
 export class HomePage {
 
-	latitude: any;
-	longitude: any;
+	latitude: Number = 0;
+	longitude: Number = 0;
+	map: any;
 
-	constructor(public navCtrl: NavController, private geolocation: Geolocation) {
-
+	constructor(
+		public navController: NavController, 
+		public platform: Platform, 
+		private geolocation: Geolocation
+	) { 
+		
 	}
 
 	ionViewWillLoad() {
-		this.geolocation.getCurrentPosition().then((resp) => {
+		this.geolocation.getCurrentPosition(options).then((resp) => {
 			this.latitude = resp.coords.latitude;
 			this.longitude = resp.coords.longitude;
 			console.log('current position')
@@ -33,6 +46,21 @@ export class HomePage {
 			console.log('watched position')
 			console.log(data.coords.longitude + ' ' + data.coords.latitude);
 		});
+	}
+
+	ionViewDidEnter() {
+		this.geolocation.getCurrentPosition(options).then((resp) => {
+			this.latitude = resp.coords.latitude;
+			this.longitude = resp.coords.longitude;
+			console.log('current position')
+			console.log(this.latitude + ' ' + this.longitude)
+			this.map = new google.maps.Map(document.getElementById('map'), {
+				center: { lat: this.latitude || 0, lng: this.longitude || 0 },
+				zoom: 15
+			});
+		}).catch((error) => {
+			console.log('Error getting location', error);
+		});		
 	}
 
 }
