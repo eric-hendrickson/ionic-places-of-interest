84781b6744548e60fc1736febc72f979a878d66e
Eric Hendrickson <hendrickson02@gmail.com>
2018-08-27 19:58:36 -0700

    Trying for Android bundling

--- native/src/pages/place/place.ts
+++ native/src/pages/place/place.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
-import { MapsProvider } from '../../providers/maps/maps';
 
 @Component({
-	selector: 'place-home',
+	selector: 'page-place',
 	templateUrl: 'place.html'
 })
 export class PlacePage {
 	constructor(
 		public navController: NavController,
-		public platform: Platform,
-		private mapsProvider: MapsProvider
+		public platform: Platform
 	) {
 
 	}
